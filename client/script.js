2061e8586037051cbe46b6d6c06ebac661b6be73
Ricky Wu <rickywu209@gmail.com>
2023-01-05 02:23:31 -0700

    update the server url for deployment

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch('http://localhost:4000', {
+  const response = await fetch('https://code-bot-0a2u.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
